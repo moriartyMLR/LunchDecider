b3d69d2465106ad2ef41573122fdba150b0dfa3e
moriartyMLR <97626090+moriartyMLR@users.noreply.github.com>
2022-08-01 11:58:11 -0400

    Created basic logic

--- app.py
+++ app.py
@@ -1,27 +1,34 @@
 from tkinter import *
 from tkinter import ttk
 from random import choice
+
+class LunchDecider:
+    def __init__(self, root):
+        root.title("Random Food Decider")
+        root.resizable(width=False, height=False)
+        root.bind("<Return>", self.get_list)
+
+        mainframe = ttk.Frame(root, padding="3 3 12 12")
+        mainframe.grid(column=0, row=0, sticky=(N,S,E,W))
+
+        self.list_choices = StringVar()
+        self.picked_option = StringVar()
+
+        ttk.Label(mainframe, text="Your Options Separated by Commas").grid(column=0, row=0, padx=10, pady=5)
+        ttk.Label(mainframe, text="Your Random Selection is:").grid(column=1, row=0, padx=10, pady=5)
+        ttk.Label(mainframe, textvariable=self.picked_option).grid(column=1, row=1)
+
+        list_box = StringVar()
+        self.list_box_entry = ttk.Entry(mainframe, textvariable=list_box)
+        self.list_box_entry.grid(column=0, row=1, padx=10, pady=10)
+
+        ttk.Button(mainframe, text="Get Random Choice", command=self.get_list).grid(column=0, row=2)
+
+    def get_list(self, *args):
+        self.list_choices = self.list_box_entry.get().replace(" ", "").split(",")
+        self.picked_option.set(choice(self.list_choices))
+
+
 root = Tk()
-root.title("Random Food Decider")
-
-random_option = []
-
-def get_list():
-    random_option = list_box_entry.get().split(",").strip()
-    print(random_option)
-
-mainframe = ttk.Frame(root, padding="3 3 12 12")
-mainframe.grid(column=0, row=0, sticky=(N,S,E,W))
-
-ttk.Label(mainframe, text="Fill in Your Options Here").grid(column=0, row=0, padx=10, pady=5)
-ttk.Label(mainframe, text="Your Random Selection is:").grid(column=1, row=0, padx=10, pady=5)
-
-list_box = StringVar()
-list_box_entry = ttk.Entry(mainframe, textvariable=list_box)
-list_box_entry.grid(column=0, row=1, padx=10, pady=10)
-
-ttk.Button(mainframe, text="Get Random Choice", command=get_list).grid(column=0, row=2)
-
-root.bind("<Return>", get_list)
-
+LunchDecider(root)
 root = mainloop()
